607399e72a9ca779f4433bf9c596a1c3ed8db111
LeonNapoleonov <127660649+LeonNapoleonov@users.noreply.github.com>
2023-03-20 00:51:10 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,57 +1,49 @@
 # python3
 # Leons Dolgopolovs 221RDB432
 
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-    n = len(data)
-    for i in range(n // 2, -1, -1):
-        swaps += heapify(data, n, i)
-    return swaps
-
-def heapify(data, n, i):
+def sift_down(arr, i, swaps):
+    min = i
     left = 2 * i + 1
     right = 2 * i + 2
-    largest = i
-    if left < n and data[left] < data[largest]:
-        largest = left
-    if right < n and data[right] < data[largest]:
-        largest = right
-    if i != largest:
-        data[i], data[largest] = data[largest], data[i]
-        swaps = [(i, largest)]
-        swaps += heapify(data, n, largest)
-        return swaps
-    return []
+    length = len(arr)
+
+    if left < length and arr[left] < arr[min]:
+        min = left
+
+    if right < length and arr[right] < arr[min]:
+        min = right
+
+    if i != min:
+        arr[i], arr[min] = arr[min], arr[i]
+        swaps.append((i, min))
+        sift_down(arr, min, swaps)
+
+def build_heap(arr):
+    swaps = []
+    n = len(arr)
+    for i in range(n // 2, -1, -1):
+        sift_down(arr, i, swaps)
+    return swaps
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    ievade = input()
+    if 'F' in ievade:
+        path = input()
+        path = "tests/" + path
+        with open(path, "r") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+            
+    if 'I' in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
